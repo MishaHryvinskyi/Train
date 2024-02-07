2cb3c03ea1ccb6428a19199f3fed92b832017718
Misha <hryvinskyim@gmail.com>
2024-02-07 02:29:29 +0200

    styled

--- src/components/Phoonebook/Filter/Filter.jsx
+++ src/components/Phoonebook/Filter/Filter.jsx
@@ -1,15 +1,18 @@
+import { FormEl } from "../Form/Form.styled";
+
 const Filter = ({ filter, changeFilter }) => {
     return (
-        <>
+        <FormEl>
             <label htmlFor="">
                 Filter by name 
                 <input 
                     type="text" 
                     value={filter} 
                     onChange={changeFilter}
+                    placeholder="Find by name"
                 />
             </label>
-        </>
+        </FormEl>
     )
 }
 
